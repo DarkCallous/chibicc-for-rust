938169495568f40943a49311d16c7fe1e7d72dc9
Rick <rickliu2015@outlook.com>
2026-01-11 01:44:05 +0800

    Step1. Compile a single number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env::{self};
+
 fn main() {
-    println!("Hello, world!");
+    let cmds: Vec<String> = env::args().collect();
+    let input = cmds.get(1).expect("should have at least one input");
+    println!(".intel_syntax noprefix\n");
+    println!(".globl main\n");
+    println!("main:\n");
+    println!("  mov rax, {}\n", input);
+    println!("  ret\n");
 }
