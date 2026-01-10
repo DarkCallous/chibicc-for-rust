264acaf1beeaf23c2f3a2d95dbc61627d643c752
Rick <rickliu2015@outlook.com>
2026-01-11 01:47:59 +0800

    Read input as isize not str. Reject non-number inputs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env::{self};
 
 fn main() {
     let cmds: Vec<String> = env::args().collect();
-    let input = cmds.get(1).expect("should have at least one input");
+    let input = cmds.get(1).expect("should have at least one input").parse::<isize>().expect("should input a number");
     println!(".intel_syntax noprefix\n");
     println!(".globl main\n");
     println!("main:\n");
