c9217864efbc6997eda2d2538985a2f27ede92da
Rick <rickliu2015@outlook.com>
2026-01-14 00:02:39 +0800

    Implemented a simple tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::env::{self};
+use chibicc_for_rust::tokenizer::*;
+
 
 fn main() {
     let cmds: Vec<String> = env::args().collect();
-    let input = cmds.get(1).expect("should have at least one input").parse::<isize>().expect("should input a number");
+    let input_str = cmds.get(1).expect("should have at least 1 input").as_str();
+    let tokens = tokenize(input_str.as_bytes());
     println!(".intel_syntax noprefix\n");
     println!(".globl main\n");
     println!("main:\n");
-    println!("  mov rax, {}\n", input);
+    let mut index = 0;
+    let num = next_num(&tokens, &mut index).expect("first token must be number");
+    println!("  mov rax, {}\n", num);
+    while index < tokens.len()  {
+        let char = next_reserve(&tokens, &mut index).expect("next token must be operator");
+        match char{
+            "+" => {
+                let num = next_num(&tokens, &mut index).expect("next token must be number");
+                println!("  add rax, {}\n", num);
+            }
+            "-" => {
+                let num = next_num(&tokens, &mut index).expect("next token must be number");
+                println!("  sub rax, {}\n", num);
+            }
+            _ => {
+                panic!("found not expected char!");
+            }
+        }
+    }
     println!("  ret\n");
 }
+
---
+++ src/tokenizer.rs
@@ -0,0 +1,72 @@
+﻿#[derive(Debug)]
+pub enum NextTokenError<'a>{
+    WrongType(&'a Token),
+}
+
+#[derive(Debug)]
+pub enum Token{
+    Num(i64),
+    Reserved(String),
+    Eof,
+}
+
+type TokenContainer = Vec<Token>;
+
+fn parse_next_number(s: &[u8], cursor: &mut usize) -> i64{
+    let mut val: i64 = 0;
+    while let Some(c) = s.get(*cursor){
+        if !c.is_ascii_digit() {break;}
+        val = val * 10 + (c - b'0') as i64;
+        *cursor += 1;
+    }
+    val
+}
+
+pub fn tokenize(s: &[u8]) -> TokenContainer{
+    let mut vec = Vec::new();
+    let mut cursor = 0;
+    while cursor < s.len(){
+        match s[cursor]{
+            c if c.is_ascii_whitespace() =>{
+                cursor += 1;    
+            }
+            b'+' | b'-' =>{
+                vec.push(Token::Reserved((s[cursor] as char).to_string()));
+                cursor += 1;
+            }
+            c if c.is_ascii_digit() =>{
+                vec.push(Token::Num(parse_next_number(s, &mut cursor)));
+            }
+            _ =>{
+                panic!("unexpected token");
+            }
+        }
+    }
+    vec
+}
+
+pub fn next_num<'a>(tokens: &'a [Token], index: &mut usize) -> Result<i64, NextTokenError<'a>>{
+    let result = match &tokens[*index]{
+        Token::Num(i) => {
+            Ok(*i)
+        }
+        c => {
+            Err(NextTokenError::WrongType(c))
+        }
+    };
+    *index += 1;
+    result
+}
+
+pub fn next_reserve<'a>(tokens: &'a [Token], index: &mut usize) -> Result<&'a str, NextTokenError<'a>>{
+    let result: Result<&'a str, NextTokenError<'a>> = match &tokens[*index]{
+        Token::Reserved(s) => {
+            Ok(&s)
+        }
+        c => {
+            Err(NextTokenError::WrongType(c))
+        }
+    };
+    *index += 1;
+    result
+}
